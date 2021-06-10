5178ba2f32700681dcbdaa6a3b14564a15bd40ed
Mason <MNienaber27@hotmail.com>
2021-06-09 20:37:53 -0700

    updated benchmarks

--- InputScript.py
+++ InputScript.py
@@ -1,25 +1,31 @@
 import csv
 from random import seed
 from random import randint
+import sys
 
-size = 1000
+size = int(sys.argv[1])
+
 pubID = [0]*size
 restaurant = [0]*size
 food_item = [0]*size
 discount = [0]*size
-data = [0]*size
 val = [0]*size
 
-seed(27)
-for i in range(1000):
-    pubID[i] = randint(0,1)
-    restaurant[i] = bin(pubID[i])[2:].zfill(1)
-    food_item[i] = bin(randint(0,3))[2:].zfill(2)
-    discount[i] = bin(randint(0,2))[2:].zfill(2)
-    val[i] = restaurant[i]+food_item[i]+discount[i]
-    data[i] = [pubID[i],val[i]]
+validSubs = []
+validSubs.append("10001")
+validSubs.append("00110")
+#validSubs.append("11000")
+
 #print(data)
-with open('/home/mason/Documents/CS237_caching_pubsub/test.csv', 'w') as f:
-  writer = csv.writer(f)
-  writer.writerows(data)
+for j in range(1,4):
+  seed(randint(0,1000))
+  data = [0]*size
+  for i in range(size):
+    pubID[i] = randint(0,1)
+    val[i] = validSubs[randint(0,1)]
+    data[i]=[pubID[i],val[i]]
+  file="/home/mason/Documents/CS237_caching_pubsub/test{benchmark}_{tmp}.csv".format(benchmark=size,tmp=j)
+  with open(file, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
 
